71c50c8c2952b2e34b8466fe901904e18afa465b
matthijspoot <matthijspoot@reshaped.nl>
2024-04-24 11:11:58 +0200

    prompt gewijzigd, geen afbeelding meer genereren

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -2,9 +2,6 @@ export const AI_NAME = "Reshaped.AI";
 export const AI_DESCRIPTION = "Reshaped.AI is an enterprise grade AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. Always answer in dutch. You must always return in markdown format.`;
 
-You have access to the following functions:
-1. create_img: You must only use the function create_img if the user asks you to create an image.`;
-
-export const NEW_CHAT_NAME = "New chat";
+export const NEW_CHAT_NAME = "Nieuwe chat";
